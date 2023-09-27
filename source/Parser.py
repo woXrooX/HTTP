80e7ac2ebfdacd0eed79bd8a43d125cf698a5bc6
woXrooX <woXrooX@gmail.com>
2023-09-27 16:47:07 +0500

    More DRYer code

--- source/Parser.py
+++ source/Parser.py
@@ -1,163 +1,146 @@
 if __name__ != "__main__":
-    import json
+	import json
 
-    class Parser:
-        request_JSON = {
-            "requestLine": {
-                "method": None,
-                "URL": None,
-                "HTTP_version": None
-            },
-            "headers": {},
-            "body": ''
-        }
+	class Parser:
+		JSON = {}
 
-        response_JSON = {
-            "statusLine": {
-                "HTTP_version": None,
-                "status_code": None,
-                "status_message": None
-            },
-            "headers": {},
-            "body": ''
-        }
+		######### APIs
+		@staticmethod
+		def parseRequest(RAW):
+			# Converting to string
+			RAW = RAW.decode()
 
-        ######### APIs
-        @staticmethod
-        def parseRequest(RAW):
-            # Converting to string
-            RAW = RAW.decode()
+			# Clean up JSON
+			Parser.JSON = {}
 
-            # Split RAW HTTP to head and body
-            head = None
-            body = None
-            head_body_splitted = RAW.split("\r\n\r\n")
+			# Split RAW HTTP to head and body
+			head = None
+			body = None
+			head_body_splitted = RAW.split("\r\n\r\n")
 
-            # If RAW HTTP is splittable to two parts then head and body exists
-            if len(head_body_splitted) == 2: head, body = head_body_splitted
+			# If RAW HTTP is splittable to two parts then head and body exists
+			if len(head_body_splitted) == 2: head, body = head_body_splitted
 
-            # Else only head exists
-            else: head = RAW
+			# Else only head exists
+			else: head = RAW
 
-            headers = head.split("\r\n")
+			headers = head.split("\r\n")
 
-            # No headers
-            if len(headers) == 0:
-                print("Invalid HTTP")
-                return False
+			# No headers
+			if len(headers) == 0:
+				print("Invalid HTTP")
+				return False
 
-            if Parser.handleRequestLine(headers[0]) is False: return False
+			Parser.JSON["requestLine"] = Parser.handleRequestLine(headers[0])
 
-            # If request line is valid then remove it from the lines
-            headers.pop(0)
+			# If request line is valid then remove it from the lines
+			headers.pop(0)
 
-            if Parser.handleRequestHeaders(headers) is False: return False
+			Parser.JSON["headers"] = Parser.handleHeaders(headers)
 
-            Parser.handleRequestBody(body)
+			Parser.JSON["body"] = Parser.handleBody(body)
 
-            # Returns parsed HTTP data as DICT
-            return Parser.request_JSON
+			# Returns parsed HTTP data as DICT
+			return Parser.JSON
 
-        @staticmethod
-        def parseResponse(RAW):
-            # Converting to string
-            RAW = RAW.decode()
+		@staticmethod
+		def parseResponse(RAW):
+			# Converting to string
+			RAW = RAW.decode()
 
-            # Split RAW HTTP to head and body
-            head = None
-            body = None
-            head_body_splitted = RAW.split("\r\n\r\n")
+			# Clean up JSON
+			Parser.JSON = {}
 
-            # If RAW HTTP is splittable to two parts then head and body exists
-            if len(head_body_splitted) == 2: head, body = head_body_splitted
+			# Split RAW HTTP to head and body
+			head = None
+			body = None
+			head_body_splitted = RAW.split("\r\n\r\n")
 
-            # Else only head exists
-            else: head = RAW
+			# If RAW HTTP is splittable to two parts then head and body exists
+			if len(head_body_splitted) == 2: head, body = head_body_splitted
 
-            headers = head.split("\r\n")
+			# Else only head exists
+			else: head = RAW
 
-            # No headers
-            if len(headers) == 0:
-                print("Invalid HTTP")
-                return False
+			headers = head.split("\r\n")
 
-            if Parser.handleStatusLine(headers[0]) is False: return False
+			# No headers
+			if len(headers) == 0:
+				print("Invalid HTTP")
+				return False
 
-            # If request line is valid then remove it from the lines
-            headers.pop(0)
+			Parser.JSON["statusLine"] = Parser.handleStatusLine(headers[0])
 
-            if Parser.handleResponseHeaders(headers) is False: return False
+			# If status line is valid then remove it from the lines
+			headers.pop(0)
 
-            Parser.handleResponseBody(body)
+			Parser.JSON["headers"] = Parser.handleHeaders(headers)
 
-            # Returns parsed HTTP data as DICT
-            return Parser.response_JSON
+			Parser.JSON["body"] = Parser.handleBody(body)
+
+			# Returns parsed HTTP data as DICT
+			return Parser.JSON
 
 
+		######### Helpers
+		#### HTTP Request
+		@staticmethod
+		def handleRequestLine(requestLine):
+			parts = requestLine.split(' ')
+			if len(parts) != 3: return {}
 
-        ######### Helpers
-        #### HTTP Request
-        @staticmethod
-        def handleRequestLine(requestLine):
-            parts = requestLine.split(' ')
-            if len(parts) != 3:
-                print("Invalid HTTP request line")
-                return False
+			JSON = {
+				"method": None,
+				"URL": None,
+				"HTTP_version": None
+			}
 
-            Parser.request_JSON["requestLine"]["method"] = parts[0]
-            Parser.request_JSON["requestLine"]["URL"] = parts[1]
-            Parser.request_JSON["requestLine"]["HTTP_version"] = parts[2]
+			JSON["method"] = parts[0]
+			JSON["URL"] = parts[1]
+			JSON["HTTP_version"] = parts[2]
 
-        @staticmethod
-        def handleRequestHeaders(headers):
-            for header in headers:
-                splitted_header = header.split(': ')
-                if len(splitted_header) != 2:
-                    print("Invalid HTTP header: ", splitted_header)
-                    return False
+			return JSON
 
-                Parser.request_JSON["headers"][splitted_header[0]] = splitted_header[1]
+		#### HTTP Response
+		def handleStatusLine(statusLine):
+			parts = statusLine.split(' ')
+			if len(parts) != 3: return {}
 
-        @staticmethod
-        def handleRequestBody(body):
-            if body is None:
-                Parser.request_JSON["body"] = ''
-                return
+			JSON = {
+				"HTTP_version": None,
+				"status_code": None,
+				"status_message": None
+			}
 
-            # Check if content type is json then set body to json data from raw body
-            if(
-                "Content-Type" in Parser.request_JSON["headers"] and
-                Parser.request_JSON["headers"]["Content-Type"] == "application/json"
-            ): Parser.request_JSON["body"] = json.loads(body)
+			JSON["HTTP_version"] = parts[0]
+			JSON["status_code"] = parts[1]
+			JSON["status_message"] = parts[2]
 
-            else: Parser.request_JSON["body"] = body
+			return JSON
 
-        #### HTTP Response
-        def handleStatusLine(statusLine):
-            parts = statusLine.split(' ')
-            if len(parts) != 3:
-                print("Invalid HTTP status line")
-                return False
+		@staticmethod
+		def handleHeaders(headers):
+			JSON = {}
 
-            Parser.response_JSON["statusLine"]["HTTP_version"] = parts[0]
-            Parser.response_JSON["statusLine"]["status_code"] = parts[1]
-            Parser.response_JSON["statusLine"]["status_message"] = parts[2]
+			for header in headers:
+				splitted_header = header.split(': ')
 
-        @staticmethod
-        def handleResponseHeaders(headers):
-            for header in headers:
-                splitted_header = header.split(': ')
-                if len(splitted_header) != 2:
-                    print("Invalid HTTP header: ", splitted_header)
-                    return False
+				# Check if invalid header
+				if len(splitted_header) != 2: continue
 
-                Parser.response_JSON["headers"][splitted_header[0]] = splitted_header[1]
+				JSON[splitted_header[0]] = splitted_header[1]
 
-        @staticmethod
-        def handleResponseBody(body):
-            if body is None:
-                Parser.response_JSON["body"] = ''
-                return
+			return JSON
 
-            Parser.response_JSON["body"] = body
+		@staticmethod
+		def handleBody(body):
+			if body is None: return ''
+
+			# Check if content type is json then set body to json data from raw body
+			if(
+				"Content-Type" in Parser.JSON["headers"] and
+				Parser.JSON["headers"]["Content-Type"] == "application/json"
+			): return json.loads(body)
+
+			else: return body
 
